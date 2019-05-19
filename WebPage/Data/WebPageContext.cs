451173f60f0571c7fba7165b2a25cecdc397b853
StudeyPC <zhzh@tongji.edu.cn>
2019-05-19 22:05:13 +0800

    Init WebPage

---
+++ WebPage/Data/WebPageContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebPage.Models
+{
+    public class WebPageContext : DbContext
+    {
+        public WebPageContext (DbContextOptions<WebPageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebPage.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ WebPage/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebPage.Models;
+
+namespace WebPage.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WebPage.Models.WebPageContext _context;
+
+        public IndexModel(WebPage.Models.WebPageContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
